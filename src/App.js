9a4020dd1df0e871bf31d4c5327d37327f171bd3
Amy Gori <amydgori@gmail.com>
2023-01-19 16:54:36 -0500

    Code from this afternoon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function App(props) {
+  // I don't _really_ need this to be in state, since as is these values don't change.
+  // State is for information that changes.
+  // But if they were going to change, which they definitely would in a real application,
+  // I'd want this info to be in state.
+  const [devs, setDevs] = useState([
+      {name: "Daniel", expertise: "JS, Beats"},
+      {name: "Cameron", expertise: "JS, Soccer"},
+      {name: "Lucian", expertise: "JS, Corny Memes"},
+      {name: "Marcus", expertise: "JS, Comic Books"},
+    ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Devs For Hire</h1>
+      <div className="dev-list" id="dev-list">
+        { devs.map(dev => <Developer name={dev.name} expertise={dev.expertise} />)}
+      </div>
     </div>
   );
+}
+
+function Developer({name, expertise}){
+  const [expanded, setExpanded] = useState(false)
+
+  const handleClick = () => setExpanded(!expanded)
+
+  return(
+    <div style={{border: 'solid 1px silver', margin: '5px', padding: '10px'}}>
+      <p>{name}</p>
+      <button onClick={handleClick}>More Info</button>
+      { expanded && <p>{expertise}</p> }
+    </div>
+  )
 }
 
 export default App;
