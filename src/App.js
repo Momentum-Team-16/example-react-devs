43656a36f460586b29711887f20ac727d1634be0
Amy Gori <amydgori@gmail.com>
2023-01-31 17:51:54 -0500

    Refactor to use API and add ability to create new dev

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import { useState, useEffect } from 'react'
 import { DateGreeting } from './components/DateGreeting'
 import axios from 'axios'
-import { Questions } from "./components/Trivia";
+import DevForm from './components/DevForm'
+import orderBy from "lodash"
 
 function App(props) {
-  // I don't _really_ need this to be in state, since as is these values don't change.
-  // State is for information that changes.
-  // But if they were going to change, which they definitely would in a real application,
-  // I'd want this info to be in state.
-  const [devs, setDevs] = useState([
-    { name: 'Daniel', expertise: 'JS, Beats', gitHub: 'villeryd' },
-    { name: 'Cameron', expertise: 'JS, Soccer', gitHub: 'cameronpoulton' },
-    { name: 'Lucian', expertise: 'JS, Corny Memes', gitHub: 'BitGitMart' },
-    { name: 'Marcus', expertise: 'JS, Comic Books', gitHub: 'marcusvno' },
-    {
-      name: 'Capel',
-      expertise: 'JS, All Things Audio',
-      gitHub: 'capelhoworth',
-    },
-  ])
-  const [selectedDev, setSelectedDev] = useState("")
+  const [devs, setDevs] = useState([])
+  const [selectedDev, setSelectedDev] = useState('')
+  useEffect(() => {
+    axios.get('https://node-api-devs-for-hire.glitch.me/devs').then(res => setDevs(res.data))
+  }, [])
 
   if (selectedDev) {
-    const dev = devs.filter(dev => dev.name === selectedDev)[0]
-    return <DeveloperDetail name={dev.name} setSelectedDev={setSelectedDev} gitHub={dev.gitHub} expertise={dev.expertise}/>
+    const dev = devs.filter((dev) => dev.name === selectedDev)[0]
+    return (
+      <DeveloperDetail
+        name={dev.name}
+        setSelectedDev={setSelectedDev}
+        gitHub={dev.gitHub}
+        expertise={dev.expertise}
+      />
+    )
+  }
+
+  const handleAddNewDev = (devObj) => {
+    setDevs(orderBy([devObj, ...devs], ['id']))
   }
 
   return (
@@ -32,19 +33,23 @@ function App(props) {
         <h1>React Devs For Hire</h1>
       </header>
       <main>
-      <DateGreeting />
+        <DateGreeting />
         <div className="dev-list">
           {devs.map((dev) => (
             <Developer
               name={dev.name}
               expertise={dev.expertise}
-              key={dev.name}
+              key={dev.id}
               gitHub={dev.gitHub}
               selectDev={setSelectedDev}
             />
           ))}
         </div>
       </main>
+      <div className="form">
+        <h2>Add a new dev</h2>
+        <DevForm addNewDev={handleAddNewDev} />
+      </div>
     </>
   )
 }
@@ -54,37 +59,45 @@ function Developer({ name, selectDev }) {
     <div style={{ border: 'solid 1px silver', margin: '5px', padding: '10px' }}>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <p>{name}</p>
-        <button className="button button-outline" onClick={() => selectDev(name)}>
+        <button
+          className="button button-outline"
+          onClick={() => selectDev(name)}
+        >
           Select This Dev
         </button>
       </div>
-
     </div>
   )
 }
 
-function DeveloperDetail({name, expertise, gitHub, setSelectedDev}) {
+function DeveloperDetail({ name, expertise, gitHub, setSelectedDev }) {
   const [repos, setRepos] = useState([])
 
   useEffect(() => {
     axios
       .get(`https://api.github.com/users/${gitHub}/repos?per_page=5`)
-      .then((response) => setRepos(response.data.map(obj => [obj.name, obj.html_url])))
+      .then((response) =>
+        setRepos(response.data.map((obj) => [obj.name, obj.html_url]))
+      )
   }, [gitHub])
 
   return (
     <>
-      <button onClick={() => setSelectedDev("")}>Back to List</button>
+      <button onClick={() => setSelectedDev('')}>Back to List</button>
       <h1>Details about {name}</h1>
       <div>
-          <>
-            <p>{expertise}</p>
-            <ul>
-              {repos.map(([repoName, repoURL]) => (
-                <li key={repoURL}><a href={repoURL} target="_blank" rel="noreferrer">{repoName}</a></li>
-              ))}
-            </ul>
-          </>
+        <>
+          <p>{expertise}</p>
+          <ul>
+            {repos.map(([repoName, repoURL]) => (
+              <li key={repoURL}>
+                <a href={repoURL} target="_blank" rel="noreferrer">
+                  {repoName}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </>
       </div>
     </>
   )
---
+++ src/components/DevForm.js
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+const DevForm = ({addNewDev}) => {
+  const [name, setName] = useState('')
+  const [expertise, setExpertise] = useState('')
+  const [github, setGithub] = useState('')
+  const [available, setAvailable] = useState(true)
+
+  const resetForm =  () => {
+    setName('')
+    setExpertise('')
+    setGithub('')
+    setAvailable('')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const devEndpoint = 'https://node-api-devs-for-hire.glitch.me/devs'
+    axios.post(devEndpoint, {
+      name, expertise, github, available
+    }, {
+      headers: {"admin_key": process.env.REACT_APP_ADMIN_KEY}
+    }).then(res => {
+      addNewDev(res.data.created)
+      resetForm()
+    }).catch(e => console.log(e))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="expertise">Expertise</label>
+        <input
+          type="text"
+          name="expertise"
+          id="expertise"
+          value={expertise}
+          onChange={(e) => setExpertise(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="github">GitHub Username</label>
+        <input
+          type="text"
+          name="github"
+          id="github"
+          value={github}
+          onChange={(e) => setGithub(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="available">
+          Available for hire
+          <input
+            type="checkbox"
+            name="available"
+            id="available"
+            checked={available}
+            onChange={(e) => setAvailable(e.target.checked)}
+          />
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default DevForm
